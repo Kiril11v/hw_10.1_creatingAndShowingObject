710b5d8012e3a86b056c0d9c248bd48525171a76
Kyrylo_Kud <kudkiril@gmail.com>
2025-07-05 11:14:36 +0200

    showObject

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+
+const user = { // object
+    name: 'Kiril',
+    age: 23,
+    phoneNumber: +480123456789,
+    city: 'Warsaw',
+    hobby: 'football',
+    mail: 'blablabla@gmail.com',
+
+showInfo() { // method(function) of object
+    console.log(`My name is ${this.name}`);
+    console.log(`I'm ${this.age} years old`);
+    console.log(`I live in ${this.city}`);
+    console.log(`My hobby is ${this.hobby}`);
+    console.log(`My number is ${this.phoneNumber}`);
+    console.log(`My email is ${this.mail}`);
+  }
+}
+
+user.showInfo();
+
+
